b60fa45e6b7f3de651813357e88b9a8d3914fb5d
George <admin@georgesimos.com>
2020-03-03 10:18:21 +0200

    revese strings Using a Decrement for loop

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,11 +4,21 @@
     reverse('Hello, world!'); // !dlrow ,olleH
 */
 
+// Using Built-in Functions
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// Using a Decrement for loop
 function reverse(str) {
-  return str
-    .split("")
-    .reverse()
-    .join("");
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 }
 
 console.log(reverse("Hello, world!")); // !dlrow ,olleH
