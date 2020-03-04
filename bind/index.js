d60ae3e9386fa047fe4b8f8f52fe4e23147faf79
George <admin@georgesimos.com>
2020-03-04 15:14:45 +0200

    bind

---
+++ bind/index.js
@@ -0,0 +1,22 @@
+// Implement Function.prototype.bind()
+
+/*
+    const foo = function() {
+        console.log(this.bar);
+    }
+    let baz = foo.bind({bar: 'hello'})
+    baz(); // Hello
+*/
+
+Function.prototype.bind = function(context) {
+  const _this = this;
+  return function() {
+    _this.call(context);
+  };
+};
+
+const foo = function() {
+  console.log(this.bar);
+};
+let baz = foo.bind({ bar: "hello" });
+baz(); // Hello
